a8826eabc34e39b5b9c5fb76a804efd2db7317ca
Hà Quang Đạt <quangdat066@gmail.com>
2023-09-16 20:29:35 +0700

    Update header css

--- InvestorliftBlazor/InvestorliftBlazor/Program.cs
+++ InvestorliftBlazor/InvestorliftBlazor/Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using InvestorliftBlazor.Data;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -10,6 +13,25 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<DatabaseService>();
 builder.Services.AddSingleton<HouseService>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie()
+    .AddGoogle(options =>
+    {
+        IConfigurationSection googleAuthNSection = config.GetSection("Authentication:Google");
+        options.ClientId = googleAuthNSection["ClientId"];
+        options.ClientSecret = googleAuthNSection["ClientSecret"];
+
+        options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+
+    })
+    .AddFacebook(options =>
+    {
+        IConfigurationSection FBAuthNSection = config.GetSection("Authentication:FB");
+        options.ClientId = FBAuthNSection["ClientId"];
+        options.ClientSecret = FBAuthNSection["ClientSecret"];
+    });
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IHttpContextAccessor>();
 
 var app = builder.Build();
 
@@ -24,6 +46,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseRouting();
 
--- InvestorliftBlazor/InvestorliftBlazor/wwwroot/js/Observer.js
+++ InvestorliftBlazor/InvestorliftBlazor/wwwroot/js/Observer.js
@@ -1,4 +1,4 @@
-﻿
+﻿// Main page function
 window.Observer = {
     observer: null,
     Initialize: function (component, observerTargetId) {
@@ -11,3 +11,36 @@ window.Observer = {
         this.observer.observe(element);
     }
 };
+
+// Detail page function
+window.onscroll = function () {
+    var btn = $("#btnTop");
+    if (btn) {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            btn.removeClass('hide-button');
+            btn.addClass('show-button');
+        } else {
+            btn.removeClass('show-button');
+            btn.addClass('hide-button');
+        }
+    }
+};
+
+backToTop = function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+scrollVertical = targetId => {
+    var target = document.getElementById(targetId);
+    if (target) {
+        target.scrollIntoView();
+    }
+};
+
+scrollHorizontal = targetId => {
+    var target = document.getElementById(targetId);
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+    }
+}
